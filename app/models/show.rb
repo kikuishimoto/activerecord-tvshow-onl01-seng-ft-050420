35bea319404e25c3272579ce856cc2b812565c4f
kiku ishimoto <kikuish@gmail.com>
2020-06-11 21:27:45 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
     self.maximum(:rating)
   end
   def self.most_popular_show
-    self.where("rating = ?", self.highest_rating)
+    self.where("rating = ?", self.highest_rating).first
   end
 
 end
